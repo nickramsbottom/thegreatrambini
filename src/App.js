7e2701f6384ba649915fa903afd8bf38360ffc78
Will Howard <will.howard@hogarthww.com>
2018-05-03 10:59:17 +0100

    Added text and corrected styling.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ const App = () => (
     <div className="frame__outer">
       <div className="dots">
         <div className="frame__inner">
+          <div>
+            <h2>Have your fortune told by...</h2>
+            <div className="spacer" />
+            <h1>The Great Rambini</h1>
+          </div>
         </div>
       </div>
     </div>
