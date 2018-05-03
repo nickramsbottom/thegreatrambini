a0f04568ddf32fcb51d86ad76b4671a5cf78bcf9
Will Howard <will.howard@hogarthww.com>
2018-05-03 11:28:54 +0100

    Added button styling and fortune cookie retrieval functionality.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-const App = () => (
-  <div className="layout">
-    <div className="background" />
-    <div className="frame__outer">
-      <div className="dots">
-        <div className="frame__inner">
-          <div>
-            <h2>Have your fortune told by...</h2>
-            <div className="spacer" />
-            <h1>The Great Rambini</h1>
+class App extends Component {
+  state = {
+    fortune: null,
+  }
+
+  getFortune = () => {
+    axios.get('https://fortunecookieapi.herokuapp.com/v1/cookie')
+      .then(res => this.setState({ fortune: res.data[0].fortune.message }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    const { fortune } = this.state;
+
+    return (
+      <div className="layout">
+        <div className="background" />
+        <div className="frame__outer">
+          <div className="dots">
+            {fortune ?
+              <div className="frame__inner">
+                <div>
+                  <h2>{ fortune }</h2>
+                  <div className="spacer__large" />
+                  <button onClick={this.getFortune}>Hit Me Again</button>
+                </div>
+              </div>
+              :
+              <div className="frame__inner">
+                <div>
+                  <h2>Receive words of wisdom from...</h2>
+                  <div className="spacer" />
+                  <h1>The Great Rambini</h1>
+                  <div className="spacer__large" />
+                  <button onClick={this.getFortune}>Let&#8217;s Go</button>
+                </div>
+              </div>
+            }
           </div>
         </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default App;
